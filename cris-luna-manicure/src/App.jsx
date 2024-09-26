baf5fa340d9d84111c54932c5ee032a64d8607fe
cassiafigueiredo <figueiredo.cassia05@gmail.com>
2024-09-26 18:22:49 -0300

    primeiro commit

---
+++ cris-luna-manicure/src/App.jsx
@@ -0,0 +1,18 @@
+import './index.css'
+import Header from './components/Header'
+import CrisLuna from './components/CrisLuna'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <CrisLuna/>
+    </>
+  )
+}
+
+export default App
