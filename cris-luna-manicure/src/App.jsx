2d0621b109817c2c64f49ac88519779a479a20b2
cassiafigueiredo <figueiredo.cassia05@gmail.com>
2024-10-14 09:07:32 -0300

    mudança de cor do background

--- cris-luna-manicure/src/App.jsx
+++ cris-luna-manicure/src/App.jsx
@@ -1,8 +1,8 @@
-import Header from './components/Header'
-import CrisLuna from './components/CrisLuna'
-import Carrossel from './components/Carrossel'
+import Header from './components/Header/Header'
+import CrisLuna from './components/CrisLuna/CrisLuna'
+import Carrossel from './components/Carrossel/Carrossel'
 import imagens from './components/ArrayImagens'
-import BemVinda from './components/BemVinda'
+import BemVinda from './components/BemVinda/BemVinda'
 import './App.css'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
