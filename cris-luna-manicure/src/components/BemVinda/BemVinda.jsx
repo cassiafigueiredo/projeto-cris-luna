7581050f84ea6e87f0e7f520c8e1005db64c9a38
cassiafigueiredo <figueiredo.cassia05@gmail.com>
2024-10-23 23:55:15 -0300

    mudança na parte inicial e media queries

--- cris-luna-manicure/src/components/BemVinda/BemVinda.jsx
+++ cris-luna-manicure/src/components/BemVinda/BemVinda.jsx
@@ -1,18 +1,18 @@
-import imagens from '../ArrayImagens';  
-import Carrossel from '../Carrossel/Carrossel'
+//import imagens from '../ArrayImagens';  
+//import Carrossel from '../Carrossel/Carrossel'
 import styles from './BemVinda.module.css'
 import PropTypes from 'prop-types'
 
 function BemVinda() {
     return(
         <div className= {`pt-5  ${styles.bemVinda}`}>
-        <Carrossel arrayImages={imagens}/>
+        
         <div className={styles.frase}>
             <h2 className={styles.bvH2}>
                 Seja Bem vinda(o)!
             </h2>
             <p className={styles.bvP}>
-                Confira alguns dos meus trabalhos e navegue pelo site para se apaixonar pelos detalhes!
+                Arrasta pra cima e vem conhecer meu trabalho!
             </p>
         </div>
     </div>
